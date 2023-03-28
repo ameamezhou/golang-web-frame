c5474ad2005d3671447c17d45c08ab2d510133f2
ameamezhou <wxz17352825505@163.com>
2023-03-29 00:33:54 +0800

    tire error check

---
+++ base4/test1/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Tire desc
+type node struct {
+	pattern     string   // 完整的路由        /xiao/qi/:zhou/test
+	part        string   // 路由中的一部分     :zhou
+	children 	[]*node  // 存储子节点
+	isWild  	bool     // 是否精确匹配       在含有: or * 的时候为 true
+}
+
+// 第一个匹配成功的节点，用于插入
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	//fmt.Println(n)
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	//fmt.Println(child == nil)
+	if child == nil {
+		//fmt.Println(1)
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+		//fmt.Println(n)
+		//fmt.Println(child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func main(){
+	a := node{
+		pattern: "/",
+		part: "",
+		children: []*node{},
+		isWild: false,
+	}
+	pathSlice := []string{
+		"/xiao/qi/:xia",
+		"/xiao/qi/zhou",
+		"/xiao/qi/*",
+	}
+	for _, v := range pathSlice {
+		parts := strings.Split(v, "/")
+		//fmt.Println(parts)
+		//fmt.Println(v)
+		a.insert(v, parts, 1)
+	}
+	fmt.Println(a)
+	for _, v1 := range a.children {
+		fmt.Println(*v1)
+		for _, v2 := range (*v1).children {
+			fmt.Println(*v2)
+			for _, v3 := range (*v2).children {
+				fmt.Println(*v3)
+			}
+		}
+	}
+
+}
--- base4/xiawuyue_base4/tire.go
+++ base4/xiawuyue_base4/tire.go
@@ -27,3 +27,55 @@ type Tire struct {
 // 这里我们添加的 isWild 这个参数 用在模糊匹配中，当我们匹配 /xiao/qi/xiawuyue/test 这个url时
 // xiao 和 qi 都能在 Tire 的子节点中精准匹配到
 
+// 首先我们要先写一个匹配单个节点的工具函数，匹配单个节点的目的是 当我们插入一个 /xiao/qi/zhou or /xiao/qi/:zhou
+// 这样的路径时，我们需要精确匹配到根路径下的某一个子节点，并且在 /qi 这个子节点中给它加入 children 中
+
+ /*
+作者原逻辑
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+这里我对这个逻辑进行了一些修改
+我这里举例三种例子：
+/xiao/qi/zhou
+/xiao/qi/:xia
+/xiao/qi/ *   （这里和go的注释冲突了  所以加了个空格）
+当我在这里需要插入一条/xiao/qi/zhou/wu/yue 这样一条路径时
+我在 /qi 的字节点下面匹配到 /:xia or / * 的时候 就会获取到这个子节点并且往下传递， 就等于说我的 /wu /yue 这两个子节点就会加到 /:xia
+或者 / * 的下面了  这是我们不希望看到的情况，所以为了避免这个情况  这里我把逻辑进行了修改
+作者的原思想我把它汇总成代码写到了 base4/test1 写了一个测试用例，作者的动态路由存在吞噬路由的可能性
+
+
+*/
+
+func startWith(part string) bool {
+	if part[0] == 58{
+		return true
+	}
+	return false
+}
+
+
+func (t *Tire) MatchChild(part string) *Tire {
+
+	for _, child := range t.children{
+		if child.part == part && !child.isWild {
+			return child
+		}
+	}
+
+	for _, child := range t.children{
+		if child.part == part && child.isWild {
+			return child
+		}
+	}
+
+	return nil
+}
+
