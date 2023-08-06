11ba804dcb67f77aa74e81c97aa2bbb5266504dc
ameamezhou <wxz17352825505@163.com>
2023-08-06 23:57:48 +0800

    base5

--- base5/xiawuyue_base5/xiawuyue.go
+++ base5/xiawuyue_base5/xiawuyue.go
@@ -8,6 +8,22 @@ type XiaWuYue struct {
 	router *router
 }
 
+/*
+我们理想的调用 Group 的方式是
+r := gee.New()
+v1 := r.Group("/v1")
+v1.GET("/", func(c *gee.Context) {
+	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+})
+拿到一个group 并在这个 group 中进行添加后续的路由
+
+那么我们就要思考一个 Group 对象需要哪些属性
+首先我们要能够记录前缀，因为这个要记录前缀  我们补充后面内容
+我们还需要知道当前分组的父组件是谁
+web框架最重要的就是中间件，所以我们还要存中间件（其实也就是对应着一个预处理的函数）
+最后我们要用一个 (*Engine.addRoute())
+*/
+
 // New 直接调用New方法构建对象
 func New() *XiaWuYue {
 	return &XiaWuYue{ router: newRouter() }
