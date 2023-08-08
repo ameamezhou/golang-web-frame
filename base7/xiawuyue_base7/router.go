e6f1dc554e4c2b162ae34af1a0fd75926426d4c9
ameamezhou <wxz17352825505@163.com>
2023-08-09 00:46:48 +0800

    base7 init

---
+++ base7/xiawuyue_base7/router.go
@@ -0,0 +1,95 @@
+package xiawuyue_base7
+
+import (
+	"net/http"
+	"strings"
+)
+
+type router struct {
+	roots	 map[string]*Tire
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{
+		roots:		make(map[string]*Tire),
+		handlers: 	make(map[string]HandlerFunc),
+	}
+}
+
+// 这里如果要再添加路由的话我们就要考虑前缀树路由的插入了
+// 所以这里我们要先将拿到的pattern进行预处理，方便我们Tire 进行insert
+func parsePattern(pattern string)[]string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router)addRoute(method string, pattern string, handleFunc HandlerFunc) {
+	// 其中method 是用来区分 get post 等方法的
+	// patter 是提到的 muxEntry 中的匹配字符串 也就是具体的路径
+	parts := parsePattern(pattern)
+
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &Tire{}
+	}
+	err := r.roots[method].insert(pattern, parts, 0)
+	if err != nil {
+		// log err     后续实现log代码
+		return
+	}
+	r.handlers[key] = handleFunc
+}
+
+// 这里get route 是要用来初始化的   见handle
+func (r *router)getRoute(method string, path string)(*Tire, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	// searchParts 找到根节点   然后判断最后记录 pattern节点的内容。
+	// 这里的逻辑是： 找到对应的前缀树节点后，要找到通配符所匹配的字符串  也就是我们说的传进来的参数
+	// 所以新建params 存储参数内容，: 开头的字符串是用来匹配某一个字符     然后*的话会匹配后面所有的字符内容
+	t := root.search(searchParts, 0)
+	if t != nil {
+		parts := parsePattern(t.pattern)
+		for i, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[i]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[i:], "/")
+				break
+			}
+		}
+		return t, params
+	}
+	return nil, nil
+}
+
+func (r *router) handle(c *Context) {
+	t, params := r.getRoute(c.Method, c.Path)
+	if t != nil {
+		c.Params = params
+		key := c.Method + "-" + t.pattern
+		c.handlers = append(c.handlers, r.handlers[key])
+	} else {
+		c.handlers = append(c.handlers, func(c *Context) {
+			c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+		})
+	}
+	c.Next()
+}
---
+++ base7/xiawuyue_base7/xiawuyue.go
@@ -0,0 +1,108 @@
+package xiawuyue_base7
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+
+/*
+我们理想的调用 Group 的方式是
+r := gee.New()
+v1 := r.Group("/v1")
+v1.GET("/", func(c *gee.Context) {
+	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+})
+拿到一个group 并在这个 group 中进行添加后续的路由
+
+那么我们就要思考一个 Group 对象需要哪些属性
+首先我们要能够记录前缀，因为这个要记录前缀  我们补充后面内容
+我们还需要知道当前分组的父组件是谁
+web框架最重要的就是中间件，所以我们还要存中间件（其实也就是对应着一个预处理的函数）
+最后我们要用一个 (*Engine.addRoute()) 来映射所有的路由跪着和 Handler。
+*/
+
+type RouterGroup struct {
+	prefix		string
+	middlewares []HandlerFunc
+	parent 		*RouterGroup
+	engine		*XiaWuYue			// all groups share a Engine(XiaWuYue) instance
+}
+
+// XiaWuYue 这里我们将Engine改为XiaWuYue
+type  XiaWuYue struct {
+	*RouterGroup
+	router 	*router
+	groups	[]*RouterGroup
+}
+
+// New 直接调用New方法构建对象
+func New() *XiaWuYue {
+	qiuWu := &XiaWuYue{ router: newRouter() }
+	qiuWu.RouterGroup = &RouterGroup{ engine: qiuWu }
+	qiuWu.groups = []*RouterGroup{ qiuWu.RouterGroup }
+
+	return qiuWu
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+
+	return newGroup
+}
+
+func (g *RouterGroup) Use(middlewares ...HandlerFunc) {
+	g.middlewares = append(g.middlewares, middlewares...)
+}
+
+// HandlerFunc 简单定义一类函数  这就是后续具体的处理方法的类型
+type HandlerFunc func(c *Context)
+
+func (g *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("roter %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
+}
+
+func (g *RouterGroup) Get(pattern string, handlerFunc HandlerFunc) {
+	g.addRoute("GET", pattern, handlerFunc)
+}
+
+func (g *RouterGroup) Post(pattern string, handleFunc HandlerFunc) {
+	g.addRoute("POST", pattern, handleFunc)
+}
+
+func (g *RouterGroup) Pull(pattern string, handleFunc HandlerFunc) {
+	g.addRoute("PULL", pattern, handleFunc)
+}
+
+func (g *RouterGroup) Delete(pattern string, handleFunc HandlerFunc) {
+	g.addRoute("DELETE", pattern, handleFunc)
+}
+
+// Run 这里将run函数独立出来，后面我们就不用再使用http包进行跑服务了  直接用xiawuyue.run就好了
+func (x *XiaWuYue) Run(addr string) {
+	http.ListenAndServe(addr, x)
+}
+
+// 这里因为我们新建了context 所以我们只需要将context传给我们抽离出来的router使用就好了
+
+func (x *XiaWuYue) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range x.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := NewContext(w, req)
+	c.handlers = middlewares
+	x.router.handle(c)
+}
